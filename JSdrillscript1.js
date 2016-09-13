21eed31521c920e3e040884c7fdd5137162b2a0f
Matt-KozLowskiB <matt.b.kozlowski@gmail.com>
2016-09-13 12:21:47 -0700

    Revert "Updated CSS, HTMLpractice1 and JSdrillscript1 files"
    This reverts commit 14b8e734529e709bea0b3b9920e0d3f59ad41d2b.

--- JSdrillscript1.js
+++ JSdrillscript1.js
@@ -2,30 +2,30 @@
 
 $(document).ready(function () {
     var BGcolor=["#ff0000", "#483D8B", "DeepSkyBlue","DarkTurquoise", "Fuchsia", "Indigo","MediumSpringGreen","MediumAquaMarine"];
-    var x = 0;                                  // color array used for menu items 2 and 3
+    var x = 0;
     var y = 0
  
     $(".item").slideUp();
-    $("#Menu").mouseenter(function () {         //slide menu down when mouse hover
+    $("#Menu").mouseenter(function () {
         $(".item").slideDown();
-        $(".item").eq(1).click(function () {    //change text color of p elements on mouseclick menu item 2
+        $(".item").eq(1).click(function () {
             $("p").css("color", BGcolor[x]);
             x++;
             if (y > 2) { ($(".item").eq(1).unbind(click)),(y=0),(x=0) };
             if (x >= 7) { (x = 0), (y++) }});
-        $(".item").eq(2).click(function ()  {               //change background color on click menu item 3
+        $(".item").eq(2).click(function ()  {
             $("body").css("background-color", BGcolor[x]);
             x++;
-            if (y > 2) { $(".item").eq(2).unbind(click) };  //limit color changes to 2 full cycles
+            if (y > 2) { $(".item").eq(2).unbind(click) };
             if (x >= 7) { (x = 0), (y++) }
         });
-        $(".item").eq(0).click(function () {                //hide whole page on mouseclick menu item 1
+        $(".item").eq(0).click(function () {
             $("main, header, footer").hide(500);
         })
         $(".item").eq(0).click(function () {
             $("main, header, footer").hide(500);
         });
-            $(".items").mouseleave(function () {            //hide menu on mouse pointer exit menu area.
+            $(".items").mouseleave(function () {
                 $(".item").slideUp(500);
             })
 
